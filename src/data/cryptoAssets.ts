73245721b1b0c33996ce91579ad0912b4af4c1b6
Sanctiexchange <sanctiexchange@gmail.com>
2026-09-17 09:42:54 +0100

    crypto asset

---
+++ src/data/cryptoAssets.ts
@@ -0,0 +1,31 @@
+import type { CryptoAsset } from "../types/crypto.types";
+
+export const cryptoAssets: CryptoAsset[] = [
+  {
+    symbol: "BTC",
+    name: "Bitcoin",
+    icon: "₿",
+    priceNGN: 163000000,
+  },
+
+  {
+    symbol: "ETH",
+    name: "Ethereum",
+    icon: "Ξ",
+    priceNGN: 6800000,
+  },
+
+  {
+    symbol: "USDT",
+    name: "Tether",
+    icon: "₮",
+    priceNGN: 1540,
+  },
+
+  {
+    symbol: "USDC",
+    name: "USD Coin",
+    icon: "$",
+    priceNGN: 1535,
+  },
+];
